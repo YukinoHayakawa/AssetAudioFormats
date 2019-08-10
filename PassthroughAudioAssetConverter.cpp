55e8913fed8992ad2c425bef8f2a24f4fa059f6f
Yukino Hayakawa <tennencoll@gmail.com>
2019-08-10 18:04:24 +0800

    add PassthroughAudioAssetConverter

---
+++ PassthroughAudioAssetConverter.cpp
@@ -0,0 +1,11 @@
+﻿#include "PassthroughAudioAssetConverter.hpp"
+
+namespace usagi
+{
+std::shared_ptr<AudioBuffer> PassthroughAudioAssetConverter::operator()(
+    AssetLoadingContext *ctx,
+    AudioBuffer &&buffer) const
+{
+    return std::make_shared<AudioBuffer>(std::move(buffer));
+}
+}
---
+++ PassthroughAudioAssetConverter.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "LibNyquistAudioAssetDecoder.hpp"
+
+namespace usagi
+{
+struct AssetLoadingContext;
+
+struct PassthroughAudioAssetConverter
+{
+    using DefaultDecoder = LibNyquistAudioAssetDecoder;
+
+    std::shared_ptr<AudioBuffer> operator()(
+        AssetLoadingContext *ctx,
+        AudioBuffer &&buffer
+    ) const;
+};
+}
